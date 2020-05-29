c26812706c23fe9f828bb946f8e6baf94e589d0c
SiddhantKumar <duckydude20@gmail.com>
2020-05-29 18:44:44 +0530

    Done Filling up Remaining Functions leaving readContinuousLines

--- FileHandling.py
+++ FileHandling.py
@@ -33,7 +33,7 @@ def createFilesIfNotExist(filePath: str):
         pass
 
 
-def writeStringAtFirstLine(string: str, filePath: str):
+def writeFirstLine(string: str, filePath: str):
     createFilesIfNotExist(filePath)
     if os.path.getsize(filePath) == 0:
         # fileIsEmpty
@@ -48,28 +48,56 @@ def writeStringAtFirstLine(string: str, filePath: str):
                     print(line, end='')
 
 
-def writeStringAtLastLine(string: str, filePath: str):
-    pass
+def writeLastLine(string: str, filePath: str):
+    # simply appending the text
+    # skipping file exist checks
+    with open(filePath, 'a') as f:
+        f.write(string + '\n')
 
 
-def writeStringAtLineSpecifiedById(string: str, filePath: str, id: str):
-    pass
+def writeLineBySpecifiedId(string: str, filePath: str, Id: str):
+    with FileInput(filePath, inplace=True) as f:
+        for line in f:
+            if line.startswith(Id):  # replace line with string passed, if line contains the specified Id
+                print(string, end='\n')
+            else:
+                print(line, end='')
 
 
 def readFirstLine(filePath: str) -> str:
     with open(filePath, 'r') as f:
         f.seek(0)
         string = f.readline()
-    return string
+    return string[:-1]  # removing last '\n' in the string
 
 
-def readLastLine(filePath: str):
-    pass
+def readLastLine(filePath: str) -> str:
+    with open(filePath, 'r') as f:
+        f.seek(0, os.SEEK_END)
+        backStep = 2
+        f.seek(f.tell() - backStep, os.SEEK_SET)
+        ch = f.read(1)
+        readLine = ch
+        while True:
+            # if (f.tell() - backStep) < 0: break skipping this check as we will always use readFirstLine to read First
+            f.seek(f.tell() - backStep, os.SEEK_SET)
+            ch = f.read(1)
+            if ch == '\n':
+                break
+            readLine = readLine + ch
+    return readLine[::-1]  # reverse the string and return
 
 
-def readLineBySpecifiedId(filePath: str, id: str):
-    pass
+def readLineBySpecifiedId(filePath: str, Id: str):
+    # we can optimize this function but we will do that later
+    with open(filePath, 'r') as f:
+        for line in f:
+            if line.startswith(Id):
+                lineString = line
+                break
+    return lineString[:-1]  # remove the last '\n'
 
 
 def readContinuousLines(filePath: str, count: int = 10, shouldSkipFirstLine: bool = False):
+    # will create this function when needed
     pass
--- tests/test_FileHandling.py
+++ tests/test_FileHandling.py
@@ -18,5 +18,31 @@ class Test(TestCase):
         self.addCleanup(lambda: shutil.rmtree("tempPath"))
         filename = "tempPath/fileName"
         string = "1====Name====123456====email@ex.com====h 43, Delhi"
-        writeStringAtFirstLine(string, filename)
-        self.assertEqual(string + '\n', readFirstLine(filename))
+        writeFirstLine(string, filename)
+        self.assertEqual(string, readFirstLine(filename))
+
+    def test_WriteAndReadLastLine(self):
+        os.mkdir("tempPath")
+        self.addCleanup(lambda: shutil.rmtree("tempPath"))
+        filename = "tempPath/fileName"
+        string = "1====Name====123456====email@ex.com====h 43, Delhi"
+        writeFirstLine("SomeRandomText", filename)  # this is necessary else the test will break
+        writeLastLine(string, filename)
+        self.assertEqual(string, readLastLine(filename))
+
+    def test_WriteThenReadThenWriteAtIdAndThenReadAtId(self):
+        os.mkdir("tempPath")
+        self.addCleanup(lambda: shutil.rmtree("tempPath"))
+        filename = "tempPath/fileName"
+        string1 = "1====Name====123456====email@ex.com====h 43, Delhi"
+        string2 = "2====Name====123456====email@ex.com====h 43, Delhi"
+        string3 = "3====Name====123456====email@ex.com====h 43, Delhi"
+        string4 = "4====Name====123456====email@ex.com====h 43, Delhi"
+        writeFirstLine(string1, filename)
+        writeLastLine(string2, filename)
+        writeLastLine(string3, filename)
+        writeLastLine(string4, filename)
+        self.assertEqual(string2, readLineBySpecifiedId(filename, "2"))
+        testString = "3====Name====12345678910====email@ex.com====h 43, Delhi"
+        writeLineBySpecifiedId(testString, filename, "3")
+        self.assertEqual(testString, readLineBySpecifiedId(filename, "3"))
